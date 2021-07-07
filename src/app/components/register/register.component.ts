144d2933d5e31e274d6ac670d9e0f74f8e45a197
bielidev <caarolbg@gmail.com>
2021-07-07 17:03:46 +0200

    fake register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  success: boolean = false;
+  error: boolean = false;
+  username: string = ''
+  email: string = ''
+  password: string = ''
+  password2: string = ''
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  registerUser(): void {
+    if (this.isFormValid()) {
+      this.success = true;
+      this.error = false;
+    } else {
+      this.success = false;
+      this.error = true;
+    }
+  }
+
+  isFormValid(): boolean {
+    return this.username && this.email && this.password && this.password2 && (this.password === this.password2)
   }
 
 }
